e181f53edcb354cb4ae00221011dc5435d54e2dd
panchis7u7 <smadrigal.rod@gmail.com>
2021-03-23 22:18:57 -0600

    Obteniendo argumentos de la linea de comandos.

--- asm/cesar.asm
+++ asm/cesar.asm
@@ -11,18 +11,33 @@ section .data
     ;El orden de los registros: 
     ;%rdi, %rsi, %rdx, %rcx, %r8 and %r9 
     ;eso significa que argc esta en %rdi y argv en %rsi.
-    argcstr db 'argc = %i\n\0'
-    argvstr db 'argv[%u] = %s\n\0'  ;%u -> unsigned int
-    mensaje db "hola %d",0xA,0
-    sys_stdout equ 1
+    formatoRotaciones db 'El numero de desplazamientos es %d.',0xA,0x0
+    formatoTexto db "El mensaje a encriptar es: %s.",0xA,0x0
+    fewArgumentsError db "Error en los argumentos!.",0xA,0x0
+    fewArgumentsErrorLen equ $-fewArgumentsError
+    STDOUT_FILENO equ 0x01
+    STDERR_FILENO equ 0x02
 section .bss
     argc resq 1
     nDesplazamientos resb 1
+    mensaje resb 1
 section .text
 global main
 main:
     push rbp                            ;Guarda el apuntador de la base de
     mov rbp, rsp                        ;la pila.
+
+    ;Verificar si se ingresaron los argumentos necesarios.
+    ;(numero de desplazamientos y el texto a cifrar).
+    ;--------------------------------------------------------------------------
+    
+    cmp rdi,3
+    jl exit_error
+
+    ;--------------------------------------------------------------------------
+
+    ;Almacenar los argumentos (numero de desplazamientos y el texto a cifrar).
+    ;--------------------------------------------------------------------------
 
     xor rax,rax                         ;Limpiamos rax.
     xor rdx, rdx                        ;Limpiamos rdx.
@@ -31,13 +46,31 @@ main:
     mov rdx, [rax]                      ;Desreferencia *argv[1].
     sub dl, '0'                         ;Convertimos de string a int.
     mov [nDesplazamientos], byte dl     ;Guardamos el numero de desplazamientos.
+    ;mov r8, [rsi+16]
+    ;mov [mensaje], r8
 
+    mov rdi, formatoRotaciones          ;Formato de la cadena.
+    mov rax, 0                          ;No se usaron registros de punto flotante
+    mov rsi, [nDesplazamientos]         ;Numero de desplazamientos en formato int
+    call printf                         ;Se llama la funcion externa 'printf.'
 
-    mov rdi, mensaje
-    mov rax, 0
-    mov rsi, [nDesplazamientos]
-    call printf
-    
+    ;mov rdi, formatoTexto               ;Formato del texto plano.
+    ;mov rax, 0                          ;No se usaron registros de punto flotante
+    ;mov rsi, [mensaje]
+    ;call printf
+
     mov rsp, rbp
     pop rbp
-ret
+    ret
+
+    ;--------------------------------------------------------------------------
+exit_error:                             ;Salida con error.
+    mov rax, 1
+    mov rdi, STDERR_FILENO              ;Llamada de systema (write), que escribe al
+    mov rsi, fewArgumentsError          ;descriptor de archivos stderr.
+    mov rdx, fewArgumentsErrorLen
+    syscall                             ;Se imprime mensaje de error.
+
+    mov rax, 60                         ;Se sale del programa de manera insatisfactoria
+    mov rdi,-1                          ;con valor de retorno de -1.
+    syscall
