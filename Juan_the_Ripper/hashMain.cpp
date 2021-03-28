0462167785ee2c1df511d80d3f0d5033841d604d
panchis7u7 <smadrigal.rod@gmail.com>
2021-03-28 01:11:53 -0600

    Obtencion de la ruta del diccionario de datos.

--- Juan_the_Ripper/hashMain.cpp
+++ Juan_the_Ripper/hashMain.cpp
@@ -1,14 +1,26 @@
 #include "SHA512.hpp"
 #include <iostream>
 #include <sstream>
+#include <string.h>
 
 //Algoritmo de SHA512, main in c++.
-extern "C" void start(SHA512& sha512, char* msg);
+extern "C" void start(SHA512& sha512, char* msg, char* dictionary);
 
 int main(int argc, char *argv[]){
 	(void)argc;
 	SHA512 sha512;
-	start(sha512, argv[1]);
+	char* dictionary;
+	//if(std::string(argv[1]).find("--wordlist=") != std::string::npos)
+	if(strstr(argv[1], "--wordlist=") != NULL){
+		int len = strlen(argv[1]);
+		char* copia = (char*)alloca(len);
+		strncpy(copia, argv[1], len);
+		char* token = strtok(copia, "=");
+		token = strtok(NULL, "=");
+		dictionary = (char*)malloc(sizeof(char)*(strlen(token)-2));
+		strncpy(dictionary, token, (strlen(token)-2));
+	}
+	start(sha512, argv[2], dictionary);
 	//std::stringstream ss;
 	//ss << argv[1];
 	//std::cout << sha512.hash(ss.str()) << std::endl;
--- Juan_the_Ripper/main.asm
+++ Juan_the_Ripper/main.asm
@@ -6,6 +6,7 @@ section .data
 section .bss
     mensajeTextoPlano resq 1            ;Almacen de la contraseña en texto plano.
     sha resq 1                          ;Almacen de la contraseña cifrada.
+    diccionario resq 1                  ;Ruta absoluta del diccionario de datos.
 
 section .text
 global start
@@ -17,24 +18,34 @@ start:
     ;--------------------------------------------------------------------------
 
     mov [mensajeTextoPlano], rsi        ;Guardar el mensaje en texto plano.
-    sub rsp, 16                         ;Espacio de sombra para *this, input.
+    mov [diccionario], rdx              ;Guardar la ruta del diccionario.
+    sub rsp, 20h                        ;Espacio de sombra para *this, input.
 
     mov rsi, [mensajeTextoPlano]        ;Pasar como argumento el mensaje.
     call _ZN6SHA5124hashEPKc            ;llamar la funcion hash(char* input).
     mov [sha], rax                      ;Guardar el hash (valor de retorno).
 
-    add rsp, 16                         ;Reestablecer la pila.
+    add rsp, 20h                        ;Reestablecer la pila.
 
     ;--------------------------------------------------------------------------
 
-    sub rsp, 24
+    sub rsp, 20h
 
     mov rax, 0
     mov rdi, formato                       ;Imprimir el borde punteado.
     mov rsi, [sha]
     call printf
 
-    add rsp, 24
+    add rsp, 20h
+
+    sub rsp, 20h
+
+    mov rax, 0
+    mov rdi, formato                       ;Imprimir el borde punteado.
+    mov rsi, [diccionario]
+    call printf
+
+    add rsp, 20h
 
     mov rsp, rbp                        ;Reestablecer el apuntador de
     pop rbp                             ;la pila.
