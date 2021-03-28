1b6ebc88a4c3e045a6d635d59df64de618f76e05
panchis7u7 <smadrigal.rod@gmail.com>
2021-03-28 02:13:25 -0600

    Apertura de archivo de diccionario.

--- Juan_the_Ripper/hashMain.cpp
+++ Juan_the_Ripper/hashMain.cpp
@@ -10,22 +10,12 @@ int main(int argc, char *argv[]){
 	(void)argc;
 	SHA512 sha512;
 	char* dictionary;
-	//if(std::string(argv[1]).find("--wordlist=") != std::string::npos)
-	if(strstr(argv[1], "--wordlist=") != NULL){
-		int len = strlen(argv[1]);
-		char* copia = (char*)alloca(len);
-		strncpy(copia, argv[1], len);
-		char* token = strtok(copia, "=");
-		token = strtok(NULL, "=");
-		dictionary = (char*)malloc(sizeof(char)*(strlen(token)-2));
-		strncpy(dictionary, token, (strlen(token)-2));
+	char* ptr = strstr(argv[1], "--wordlist=");
+	if(ptr != NULL){
+		int len = strlen(ptr+11);
+		dictionary = (char*)malloc(sizeof(char)*len);
+		strncpy(dictionary, ptr+11, len);
 	}
 	start(sha512, argv[2], dictionary);
-	//std::stringstream ss;
-	//ss << argv[1];
-	//std::cout << sha512.hash(ss.str()) << std::endl;
-	//std::cout << sha512.hash(std::string(argv[1])) << std::endl;
-	//const char* msg = sha512.hash(argv[1]);
-	//printf("%s\n", msg);
 	return 0;
 }
--- Juan_the_Ripper/main.asm
+++ Juan_the_Ripper/main.asm
@@ -1,7 +1,57 @@
 extern _ZN6SHA5124hashEPKc              ;Funcion para hashear.
 extern printf                           ;Funcion para imprimir.
 section .data
+    CREATE 		equ 	1               ;Expresiones usadas para condicionales.
+	OVERWRITE 	equ 	1
+	APPEND 		equ 	1
+	O_WRITE 	equ 	1
+	READ 		equ 	1
+	O_READ 		equ 	1
+	DELETE 		equ 	1
+
+    NR_read  	equ 	0               ;Simbolos para llamadas al sistema.
+    NR_write 	equ 	1 
+    NR_open 	equ 	2 
+    NR_close 	equ 	3
+    NR_lseek 	equ 	8 
+    NR_create 	equ 	85
+    NR_unlink 	equ 	87
+
+	O_CREAT 	equ 	00000100q       ;Banderas de creacion y de estado.  
+    O_APPEND 	equ 	00002000q
+  
+    O_RDONLY 	equ 	000000q         ;Modos de acceso.
+    O_WRONLY 	equ 	000001q 
+    O_RDWR 		equ 	000002q
+
+    S_IRUSR 	equ 	00400q          ;Permiso de lectura para el usuario.
+    S_IWUSR 	equ 	00200q 	        ;Permiso de escritura para el usuario.
+ 
+    NL	  		equ		0xa					
+    bufferlen  	equ 	64
+
+    FD 		 dq 	0	                ;File descriptor
+
+    error_Close 	db "Error al cerrar el archivo!",NL,0
+    error_Write 	db "Error al escribir al archivo!",NL,0
+    error_Open 		db "Error al abrir archivo!",NL,0
+    error_Append 	db "error appending to file",NL,0
+    error_Delete 	db "error deleting file",NL,0
+    error_Read 		db "Error al leer el archivo!",NL,0
+    error_Print 	db "error printing string",NL,0
+    error_Position 	db "error positioning in file",NL,0
+        
+    success_Create 	db "File created and opened",NL,0
+    success_Close 	db "File closed",NL,NL,0
+    success_Write 	db "Written to file",NL,0
+    success_Open 	db "File opened for reading/(over)writing/updating",NL,0
+    success_Append 	db "File opened for appending",NL,0
+    success_Delete 	db "File deleted",NL,0
+    success_Read 	db "Reading file",NL,0
+    success_Position db "Positioned in file",NL,0
+
     formato db "%s",0xA,0x0             ;Formato simple para hacer debug.
+    archivoCargado db "Archivo cargado: %s",0xA,0x0
 
 section .bss
     mensajeTextoPlano resq 1            ;Almacen de la contraseña en texto plano.
@@ -29,6 +79,28 @@ start:
 
     ;--------------------------------------------------------------------------
 
+    ;Abrir el archivo.
+    ;--------------------------------------------------------------------------
+
+    mov rax, NR_open                    ;Syscall Open -> 2.
+    mov rdi, [diccionario]              ;Ruta del archivo.
+    mov rsi, O_RDWR                     ;Abrir en modo Lectura/Escritura.
+    syscall
+
+    cmp rax, 0                          ;Checar si hubo un error.
+    jl openError 
+
+    sub rsp, 20h
+
+    mov rax, 0
+    mov rdi, archivoCargado             ;Imprimir el borde punteado.
+    mov rsi, [diccionario]
+    call printf
+
+    add rsp, 20h
+
+    ;--------------------------------------------------------------------------
+
     sub rsp, 20h
 
     mov rax, 0
@@ -38,15 +110,41 @@ start:
 
     add rsp, 20h
 
-    sub rsp, 20h
-
-    mov rax, 0
-    mov rdi, formato                       ;Imprimir el borde punteado.
-    mov rsi, [diccionario]
-    call printf
-
-    add rsp, 20h
-
     mov rsp, rbp                        ;Reestablecer el apuntador de
     pop rbp                             ;la pila.
     ret
+
+    ;Errores.
+    ;--------------------------------------------------------------------------
+openError:
+    mov rdi, error_Open
+    call printString 
+
+    ;--------------------------------------------------------------------------
+
+    ;Impresion.
+    ;--------------------------------------------------------------------------
+global printString
+printString:
+
+; Count characters 
+    mov 	r12, rdi
+    mov 	rdx, 0 
+strLoop:
+    cmp 	byte [r12], 0
+    je 		strDone
+    inc 	rdx                 ;length in rdx
+    inc 	r12
+    jmp 	strLoop
+strDone:
+    cmp 	rdx, 0              ; no string (0 length)
+    je 		prtDone
+    mov 	rsi,rdi
+    mov 	rax, 1 
+    mov 	rdi, 1
+    syscall
+prtDone:
+
+    ret
+
+    ;--------------------------------------------------------------------------
