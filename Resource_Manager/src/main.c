cea4d8e8c2e60e01d7652d405bb1701f2be1cbd6
panchis7u7 <smadrigal.rod@gmail.com>
2021-06-15 23:11:11 -0500

    Created blank window in c.

---
+++ Resource_Manager/src/main.c
@@ -0,0 +1,24 @@
+#include <gtk-3.0/gtk/gtk.h>
+
+int main(int argc, char* argv[]){
+
+    GtkBuilder* builder;
+    GtkWidget* window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder, "glade/window_main.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    g_object_unref(builder);
+    gtk_widget_show(window);
+    gtk_main();
+    return 0;
+}
+
+void on_window_main_destroy(){
+    gtk_main_quit();
+}
