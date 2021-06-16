163547277f854746d66a4fead678f5c1d96233e7
panchis7u7 <smadrigal.rod@gmail.com>
2021-06-16 16:15:54 -0500

    GTK 4 implemented.

--- Resource_Manager/src/main.c
+++ Resource_Manager/src/main.c
@@ -1,24 +1,41 @@
-#include <gtk-3.0/gtk/gtk.h>
+#include <gtk-4.0/gtk/gtk.h>
 
-int main(int argc, char* argv[]){
-
-    GtkBuilder* builder;
-    GtkWidget* window;
-
-    gtk_init(&argc, &argv);
-
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "glade/window_main.glade", NULL);
-
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
-    gtk_builder_connect_signals(builder, NULL);
-
-    g_object_unref(builder);
-    gtk_widget_show(window);
-    gtk_main();
-    return 0;
+static void print_hello (GtkWidget *widget, gpointer data) {
+    (void)widget;
+    (void)data;
+  g_print ("Hello World\n");
 }
 
-void on_window_main_destroy(){
-    gtk_main_quit();
+static void activate (GtkApplication *app, gpointer user_data) {
+    (void)app;
+    (void)user_data;
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *box;
+
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Window");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+    box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_window_set_child (GTK_WINDOW (window), box);
+
+    button = gtk_button_new_with_label ("Hello World");
+    g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+    g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
+    gtk_box_append (GTK_BOX (box), button);
+
+    gtk_widget_show (window);
+}
+
+int main (int argc, char* argv[]) {
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
